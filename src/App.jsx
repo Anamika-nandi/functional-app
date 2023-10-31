18bbc3972c1912170bb0021301c6edce41058ac8
anamika-nandi <nandianamika143@gmail.com>
2023-10-31 20:15:49 +0530

    changes added

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,7 @@ function App() {
     <Box>
       <ChakraProvider theme={theme}>
         <Nav />
-        {/* 
-        <Home /> */}
+
         <HeroSec />
         <Clients />
         <FeatureSection />
--- src/pages/home/Home.jsx
+++
@@ -1,9 +0,0 @@
-import { Box, Text } from "@chakra-ui/react";
-
-export const Home = () => {
-  return (
-    <Box>
-      <Text color="red">jjjj</Text>
-    </Box>
-  );
-};
