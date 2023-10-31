344722845a0af7221253d8f1b0ee5822eebb0f8b
Anshu Raj <50476777+anshuopinion@users.noreply.github.com>
2023-10-31 19:43:30 +0530

    fonts

--- src/theme.jsx
+++ src/theme.jsx
@@ -1,6 +1,10 @@
 import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
+  fonts: {
+    heading: '"IBM Plex Sans", sans-serif',
+    body: '"IBM Plex Sans", sans-serif',
+  },
   components: {
     Button: {
       variants: {
