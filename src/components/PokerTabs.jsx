49f71def14f7f9ccf331aeb360cb4d5742c4dff4
Anshu Raj <50476777+anshuopinion@users.noreply.github.com>
2023-11-07 09:37:17 +0530

    tabs adjusted

--- src/components/PokerTabs.jsx
+++ src/components/PokerTabs.jsx
@@ -10,6 +10,8 @@ import {
   VStack,
   Icon,
   Grid,
+  Text,
+  Flex,
 } from "@chakra-ui/react";
 import React from "react";
 import { AiFillCaretDown } from "react-icons/ai";
@@ -21,26 +23,67 @@ const PokerTabs = () => {
     const styles = useMultiStyleConfig("Tabs", tabProps);
 
     return (
-      <VStack spacing="0">
+      <VStack spacing="1">
         <Grid placeItems="center" boxSize="6">
-          {" "}
           {isSelected ? <Icon as={AiFillCaretDown} boxSize="6" /> : <Box />}
         </Grid>
-        <Button __css={styles.tab} {...tabProps}>
+        <Button
+          color={isSelected ? "black" : ""}
+          fontWeight="bold"
+          bg={isSelected ? "red" : ""}
+          sx={{
+            "&:hover": {
+              bg: "red",
+              color: "black",
+            },
+            "& > p": {
+              color: isSelected ? "white" : "",
+            },
+          }}
+          borderBottomColor={isSelected ? "red" : ""}
+          __css={styles.tab}
+          {...tabProps}
+        >
           {tabProps.children}
         </Button>
       </VStack>
     );
   });
 
+  const tabs = [
+    {
+      name: "1",
+      component: <></>,
+    },
+    {
+      name: "2",
+      component: <></>,
+    },
+    {
+      name: "3",
+      component: <></>,
+    },
+    {
+      name: "4",
+      component: <></>,
+    },
+    {
+      name: "5",
+      component: <></>,
+    },
+  ];
+
   return (
     <Tabs>
-      <TabList>
-        <CustomTab>1</CustomTab>
-        <CustomTab>2</CustomTab>
-        <CustomTab>3</CustomTab>
-      </TabList>
-
+      <Flex justify="end">
+        <TabList>
+          {tabs.map((tab) => (
+            <CustomTab key={tab.name}>
+              <Text>{tab.name}</Text>
+            </CustomTab>
+          ))}
+        </TabList>
+      </Flex>
       <TabPanels>
         <TabPanel>
           <p>one!</p>
